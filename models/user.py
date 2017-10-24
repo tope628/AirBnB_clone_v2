09b331ced9f62a4fb651623127b9a795196ea499
Tope <tope628@gmail.com>
2017-10-24 09:26:05 +0000

    user db storage

--- models/user.py
+++ models/user.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 """ holds class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """Representation of a user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+       # cities = relationship("City", backref="state", cascade="delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
