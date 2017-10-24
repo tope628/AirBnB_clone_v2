ded6562fa21b83129952c4c494369db607221cd4
Tope <tope628@gmail.com>
2017-10-24 10:53:29 +0000

    delete undo

--- models/user.py
+++ models/user.py
@@ -15,6 +15,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="delete")
         reviews = relationship("Review", backref="user", cascade="delete")
     else:
         email = ""
