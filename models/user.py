857cc0c55e1e2a5fca760d99e5a9cf95dc6bd230
Tope <tope628@gmail.com>
2017-10-24 10:12:53 +0000

    nullable made true for names

--- models/user.py
+++ models/user.py
@@ -12,8 +12,8 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
        # cities = relationship("City", backref="state", cascade="delete")
     else:
         email = ""
