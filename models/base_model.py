42f613de023b04adc006ee0094f0e74ce0924802
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-14 21:41:16 +0000

    updated base_model to prepare for using SQLALchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,22 @@
 """
 Contains class BaseModel
 """
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey, Datetime
 from datetime import datetime
 import models
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
-
+Base = declarative_base()
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
+
+    id = Column(String(60), primary_key=True)
+    '''CHECK: DATETIME CAN"T BE NULL'''
+    created_at = Column(Datetime, default=datetime.utcnow)
+    updated_at = Column(Datetime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -27,7 +33,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
             models.storage.save()
 
     def __str__(self):
@@ -38,6 +43,7 @@ class BaseModel:
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -48,4 +54,12 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        try:
+            del new_dict[_sa_instance_state]
+        except KeyError:
+            pass
         return new_dict
+
+    def delete(self):
+        """delete the current instance from the storage (models.storage)"""
+        models.storage.delete()
