339c8e34810723d55ff4cf51cc55d7b652cf595b
Tanya Kryukova <ttkryukova@gmail.com>
2017-11-21 05:57:08 +0000

    script tested & passed shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,21 +7,21 @@ sudo apt-get -y install nginx
 sudo service nginx start
 
 # create folders if not already exist
-mkdir --parents /data/web_static/releases/test/
-mkdir --parents /data/web_static/shared/
+sudo mkdir --parents /data/web_static/releases/test/
+sudo mkdir --parents /data/web_static/shared/
 
 # create a fake html file for testing
 cd /data/web_static/releases/test/
-wget https://github.com/tanyastropheus/AirBnB_clone_v2/blob/master/index.html
+sudo wget https://raw.githubusercontent.com/tanyastropheus/AirBnB_clone_v2/master/index.html
 
 # delete symlink if it exists
 file="/data/web_static/current"
-if [-L "$file"]
+if [ -L "$file" ]
 then unlink "$file"
 fi
 
 # re-establish symlink /data/web_static/current to target
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # change ownership (user & group) for the /data/ directory & sub-directories
 sudo chown -R ubuntu:ubuntu /data/
@@ -29,5 +29,5 @@ sudo chown -R ubuntu:ubuntu /data/
 # configure Nginx to serve proper web content
 cd /etc/nginx/sites-available
 sudo rm -f default
-wget https://github.com/tanyastropheus/AirBnB_clone_v2/blob/master/default
+sudo wget https://raw.githubusercontent.com/tanyastropheus/AirBnB_clone_v2/master/default
 sudo service nginx restart
